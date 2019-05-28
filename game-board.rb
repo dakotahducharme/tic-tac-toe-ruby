c356eb24bc7731aedf972e82c5971872b6f5020c
Dakotah Ducharme <dakotahbducharme@gmail.com>
2019-05-28 13:51:13 -0700

    init commit

---
+++ game-board.rb
@@ -0,0 +1,8 @@
+
+puts "  1    2   3"
+puts "a    |   |   "
+puts "  ___|___|___"
+puts "b    |   |   "
+puts "  ___|___|___"
+puts "c    |   |   "
+puts "     |   |   "
---
+++ terminal.rb
@@ -0,0 +1,8 @@
+puts "Wanna play tic tac toe?"
+answer = gets.strip
+  if (answer == "yes")
+    puts "Cool! I'll be X's"
+    puts ""
+  else 
+    puts "Rude, but fine, Seeya!"
+  end
